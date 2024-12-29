ca1545d84eabac1d7adfd75c64a0decd7d8069ee
Raj Mahajan <rsmahajan_ar@jspmrscoe.edu.in>
2024-12-29 18:30:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def create_app():
     app.register_blueprint(main_blueprint)
 
     return app
-
+app = create_app()
 if __name__ == "__main__":
     app = create_app()
     app.run(debug=True , port=9000)
