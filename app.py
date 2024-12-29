8fde1de817be35fe58e4b302cc94dbb154d75c5f
Raj Mahajan <rsmahajan_ar@jspmrscoe.edu.in>
2024-12-29 17:45:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,10 +34,6 @@ def create_app():
 
     return app
 
-@app.route('/favicon.ico') 
-def favicon(): 
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
 if __name__ == "__main__":
     app = create_app()
     app.run(debug=True , port=9000)
